6768e31e67ed0f9106471c318e9aaf2a5b9548df
Kunal Das <kd.kunaldas92@gmail.com>
2026-07-15 22:51:14 +0530

    implements insert in min heap

--- main_test.go
+++ main_test.go
@@ -45,3 +45,52 @@ func TestHeapify(t *testing.T) {
 		})
 	}
 }
+
+func TestExtractMin(t *testing.T) {
+	testCases := []struct {
+		name   string
+		input  []int
+		min    int
+		output []int
+		err    error
+	}{
+		{
+			name:   "general case",
+			input:  []int{1, 4, 3, 5, 10},
+			output: []int{3, 4, 10, 5},
+			min:    1,
+			err:    nil,
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.name, func(t *testing.T) {
+			min, arr, err := ExtractMin(tC.input)
+			require.Equal(t, tC.min, min)
+			require.Equal(t, tC.output, arr)
+			require.Equal(t, tC.err, err)
+		})
+	}
+}
+
+func TestInsert(t *testing.T) {
+	tests := []struct {
+		name   string
+		input  []int
+		value  int
+		output []int
+	}{
+		{
+			name:   "happy path",
+			input:  []int{1, 4, 3, 5, 10},
+			value:  2,
+			output: []int{1, 4, 2, 5, 10, 3},
+		},
+	}
+
+	for _, tC := range tests {
+		t.Run(tC.name, func(t *testing.T) {
+			actual := Insert(tC.input, tC.value)
+			require.Equal(t, tC.output, actual)
+		})
+	}
+}
