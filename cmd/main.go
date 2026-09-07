c2f9c839b4257208cc6e2d40bf122d8f82b38aaa
Kunal Das <kd.kunaldas92@gmail.com>
2026-09-07 19:43:09 +0530

    LC-33(revise):search-in-rotated-sorted-array

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,33 @@
 package main
 
-func findKRotation(nums []int) int {
+func search(nums []int, target int) int {
+	pivot := minIndex(nums)
+
+	if nums[pivot] == target {
+		return pivot
+	} else if nums[pivot] < target && target <= nums[len(nums)-1] {
+		return binarySearch(nums, pivot+1, len(nums)-1, target)
+	} else {
+		return binarySearch(nums, 0, pivot-1, target)
+	}
+}
+
+func binarySearch(nums []int, lo, hi, target int) int {
+	for lo <= hi {
+		mid := lo + (hi-lo)/2
+
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] > target {
+			hi = mid - 1
+		} else {
+			lo = mid + 1
+		}
+	}
+	return -1
+}
+
+func minIndex(nums []int) int {
 	lo, hi := 0, len(nums)-1
 
 	for lo < hi {
--- cmd/main_test.go
+++ cmd/main_test.go
@@ -6,25 +6,33 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestFindKRotation(t *testing.T) {
+func TestSearch(t *testing.T) {
 	testCases := []struct {
 		desc   string
 		nums   []int
+		target int
 		expect int
 	}{
 		{
-			desc:   "TC0: it should return 1",
-			nums:   []int{5, 1, 2, 3, 4},
+			desc:   "TC0: it should return 4",
+			nums:   []int{4, 5, 6, 7, 0, 1, 2},
+			target: 0,
+			expect: 4,
+		}, {
+			desc:   "TC1: it should return 1",
+			nums:   []int{4, 5, 6, 7, 0, 1, 2},
+			target: 5,
 			expect: 1,
 		}, {
-			desc:   "TC0: it should return 0",
-			nums:   []int{1, 2, 3, 4, 5},
-			expect: 0,
+			desc:   "TC2: it should return -1",
+			nums:   []int{4, 5, 6, 7, 0, 1, 2},
+			target: 3,
+			expect: -1,
 		},
 	}
 	for _, tC := range testCases {
 		t.Run(tC.desc, func(t *testing.T) {
-			actual := findKRotation(tC.nums)
+			actual := search(tC.nums, tC.target)
 			require.Equal(t, tC.expect, actual)
 		})
 	}
